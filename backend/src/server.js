32a242dd246baed1239c5b05573ab4690777c524
user_acronus <user_acronus@senacsp.edu.br>
2019-10-29 18:44:32 -0300

    Incluso linha de conexão com banco de dados

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const e = require("express");
-const routes = require("./routes")
+const routes = require("./routes");
+const mongoose = require("mongoose");
 const app = e();
 
+mongoose.connect("mongodb+srv://sistemasalao:sistemaprotect@sistemasalao-zsouo.mongodb.net/sistemasalao?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 app.use(routes);
 
 app.listen(3000, () => {
